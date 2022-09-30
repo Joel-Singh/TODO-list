e1887f3c829f40ac13f26791ee70435f3cc73566
Joel Singh <joelsingh788@gmail.com>
2022-09-29 22:34:17 -0400

    Add makeProject function

--- src/project.js
+++ src/project.js
@@ -1,3 +1,19 @@
 const makeTask = (title, description, dueDate, priority) => {
   return { title, description, dueDate, priority };
 };
+
+const makeProject = (name) => {
+  return {
+    name,
+    taskArray: [],
+    addTask: function(title, description, dueDate, priority) {
+      this.taskArray.push(makeTask(title, description, dueDate, priority));
+    },
+    removeTask: function(index) {
+      this.taskArray.splice(index, 1);
+    },
+    getTasks: function() {
+      return this.taskArray;
+    }
+  }
+}
