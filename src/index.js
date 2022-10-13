37318fc58e3f9037aaf3a388d2d9a8219cab816a
Joel Singh <joelsingh788@gmail.com>
2022-10-13 10:08:20 -0400

    Add event listeners to switch between input and add button for new project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import { makeTask } from './project.js'
+
+const newProjectBtn = document.querySelector('#new-project')
+const insNewProjName = document.querySelector('.insert-new-project-name')
+const confirmBtn = document.querySelector('.confirm-btn')
+
+function setUpEventListeners() {
+  newProjectBtn.addEventListener('click', () => {
+    newProjectBtn.setAttribute('style', 'display: none')
+    insNewProjName.setAttribute('style', 'display: flex')
+  })
+
+  confirmBtn.addEventListener('click', () => {
+    newProjectBtn.setAttribute('style', 'display: block')
+    insNewProjName.setAttribute('style', 'display: none')
+  })
+}
+
+setUpEventListeners();
