7a53af1c943b29b2bd9da07df08ec0e81eb32f4c
Joel Singh <joelsingh788@gmail.com>
2022-10-15 11:48:06 -0400

    Add an ID value to projects

--- src/project.js
+++ src/project.js
@@ -1,15 +1,18 @@
 import { createTaskDomElement, displayTasksOnDOM } from "./DOM.js"
-let id = 0;
+let taskID = 0;
 const makeTask = (title, description, dueDate, priority) => {
-  id++;
+  taskID++;
   return {
-    title, description, dueDate, priority, id
+    title, description, dueDate, priority, id : taskID
   };
 };
 
+let projID = 0;
 const makeProject = (name) => {
+  projID++;
   return {
     name,
+    id : projID,
     taskArray: [],
     addTask: function(title, description, dueDate, priority) {
       this.taskArray.push(makeTask(title, description, dueDate, priority));
