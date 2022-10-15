d22ebdbaf8fbd36765628a819b81e0adf7f15f2d
Joel Singh <joelsingh788@gmail.com>
2022-10-15 17:04:33 -0400

    Move newProjectEventListeners() into eventListeners.js

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,5 +1,5 @@
 import { allProjects } from "./globalVariables"
-import { displayTasksOnDOM } from "./DOM.js"
+import { displayTasksOnDOM, addProject } from "./DOM.js"
 
 function displayTasksOnDOMEventListener(e) {
   displayTasksOnDOM(
@@ -9,4 +9,20 @@ function displayTasksOnDOMEventListener(e) {
   )
 }
 
-export { displayTasksOnDOMEventListener }
+function newProjectEventListeners() {
+  const insNewProjName = document.querySelector('.insert-new-project-name')
+  const confirmBtn = document.querySelector('.confirm-btn')
+  const newProjectBtn = document.querySelector('#new-project')
+  newProjectBtn.addEventListener('click', () => {
+    newProjectBtn.setAttribute('style', 'display: none')
+    insNewProjName.setAttribute('style', 'display: flex')
+  })
+
+  confirmBtn.addEventListener('click', () => {
+    newProjectBtn.setAttribute('style', 'display: block')
+    insNewProjName.setAttribute('style', 'display: none')
+    addProject(document.querySelector('.new-project-name-input').value)
+  })
+}
+
+export { displayTasksOnDOMEventListener, newProjectEventListeners }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import { addProject } from './DOM.js'
-
-function newProjectEventListeners() {
-  const insNewProjName = document.querySelector('.insert-new-project-name')
-  const confirmBtn = document.querySelector('.confirm-btn')
-  const newProjectBtn = document.querySelector('#new-project')
-  newProjectBtn.addEventListener('click', () => {
-    newProjectBtn.setAttribute('style', 'display: none')
-    insNewProjName.setAttribute('style', 'display: flex')
-  })
-
-  confirmBtn.addEventListener('click', () => {
-    newProjectBtn.setAttribute('style', 'display: block')
-    insNewProjName.setAttribute('style', 'display: none')
-    addProject(document.querySelector('.new-project-name-input').value)
-  })
-}
+import { newProjectEventListeners } from './eventListeners.js'
 
 newProjectEventListeners()
 addProject('balls')
