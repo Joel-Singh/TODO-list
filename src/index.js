3c22a8c2242145c7c29d306a41dfc98365dc2dc0
Joel Singh <joelsingh788@gmail.com>
2022-10-19 15:34:02 -0400

    Add second default project

--- src/index.js
+++ src/index.js
@@ -20,3 +20,9 @@ defaultProject.addTask(`Wash the dishes`, `The dishes haven't been washed and th
 defaultProject.addTask(`Complete Module 1.05 for precalculus`, `Has to do with calculating the derivative, finding the slope of y=mx+b, and other math sounding words`, `10/18/2022`, `urgent`)
 defaultProject.addTask(`Complete Unit 3 Exam for my online economics class`, `Need to review supply and demand first`, `10/19/2022`, `very-urgent`)
 displayProjectWithTasksOnDOM(defaultProject)
+
+
+const anotherProject = addProject('Another Project')
+anotherProject.addTask(`2Wash the dishes`, `The dishes haven't been washed and the damn roommates won't do em`, `10/15/2022`, `not-urgent`)
+anotherProject.addTask(`2Complete Module 1.05 for precalculus`, `Has to do with calculating the derivative, finding the slope of y=mx+b, and other math sounding words`, `10/18/2022`, `urgent`)
+anotherProject.addTask(`2Complete Unit 3 Exam for my online economics class`, `Need to review supply and demand first`, `10/19/2022`, `very-urgent`)
