522213823806fbcca9a08f2bc1965b7bed6f5be5
Joel Singh <joelsingh788@gmail.com>
2022-10-23 08:47:34 -0400

    Format project.js

--- src/project.js
+++ src/project.js
@@ -1,39 +1,43 @@
 const allProjects = []
 let currentlyDisplayedProject = null
 
-let taskID = 0;
+let taskID = 0
 const makeTask = (title, description, dueDate, priority) => {
-  taskID++;
+  taskID++
   return {
-    title, description, dueDate, priority, id: taskID
-  };
-};
+    title,
+    description,
+    dueDate,
+    priority,
+    id: taskID,
+  }
+}
 
-let projID = 0;
+let projID = 0
 const makeProject = (name) => {
-  projID++;
+  projID++
   return {
     name,
     id: projID,
     taskArray: [],
     addTask: function(title, description, dueDate, priority) {
-      this.taskArray.push(makeTask(title, description, dueDate, priority));
+      this.taskArray.push(makeTask(title, description, dueDate, priority))
     },
     addTaskFromObject: function(taskObject) {
-      this.taskArray.push(taskObject);
+      this.taskArray.push(taskObject)
     },
     removeTask: function(index) {
-      this.taskArray.splice(index, 1);
+      this.taskArray.splice(index, 1)
     },
     getTasks: function() {
-      return this.taskArray;
-    }
+      return this.taskArray
+    },
   }
 }
 
 // I don't know how else to create a exported variable that is mutable
 function setCurrentlyDisplayedProject(project) {
-  currentlyDisplayedProject = project;
+  currentlyDisplayedProject = project
 }
 
 function getProject(id) {
@@ -50,7 +54,12 @@ function getAllTasks() {
   return allTasks
 }
 
-const getTask = id => getAllTasks().find(task => task.id == id)
+const getTask = (id) => getAllTasks().find((task) => task.id == id)
 
 export { makeProject, makeTask, getProject, getTask }
-export { allProjects, currentlyDisplayedProject, setCurrentlyDisplayedProject, getAllTasks }
+export {
+  allProjects,
+  currentlyDisplayedProject,
+  setCurrentlyDisplayedProject,
+  getAllTasks,
+}
